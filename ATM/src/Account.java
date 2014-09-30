0bb3aa016488e3924e2a0ff7af76196fb36c86a6
Admin <Admin@192.168.0.20>
2014-09-30 20:38:58 +0300

    Initial classes

---
+++ ATM/src/Account.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+
+public class Account implements Serializable{
+	private String ownerName;
+	private String cardCode;//XXXX-XXXX-XXXX-XXXX format
+	private String password;
+	
+	public Account(String name, String code, String password) {
+		this.ownerName = name;
+		this.cardCode = code;
+		this.password = password;
+	}
+	
+	public boolean authorize(String cardCode, String password) {
+		return this.cardCode.equals(cardCode) && this.password.equals(password);
+	}
+	
+	public String getName() {
+		return ownerName;
+	}
+	
+	public String toString() {
+		return ownerName + " " + cardCode;
+	}
+
+}
