7c021e4de9a6fb6b020cc1fa57b5a8313cf1f67f
LynxUA <burlakovden@gmail.com>
2014-10-10 16:44:58 +0300

    added atm.graphic

---
+++ ATM/src/com/nibu/atm/graphic/ATM.java
@@ -0,0 +1,65 @@
+package com.nibu.atm.graphic;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+
+public class ATM extends JFrame{
+
+	public ATM() {
+		Toolkit kit=Toolkit.getDefaultToolkit();
+		Dimension screenSize=kit.getScreenSize();
+		int screenHeight=screenSize.height;
+		int screenWidth=screenSize.width;
+		setSize(screenWidth/2,screenHeight/2);
+		setLocation(screenWidth/4,screenHeight/4);
+		setTitle("Банкомат банка \"Nibu\"");
+		final JPanel panel = new JPanel();
+		final JTextField login = new JTextField("Введіть логін");
+		
+		final JPasswordField password = new JPasswordField();
+		//field.set
+		JButton button = new JButton("OK");
+		ActionListener listener = new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			//JOptionPane.showMessageDialog(panel, "Hello, "+field.getText());
+				ATM.this.remove(panel);
+				JPanel panel1 = new JPanel();
+				panel1.add(new JLabel(" Hello, "+login.getText()));
+				ATM.this.add(panel1);
+				ATM.this.setVisible(true);
+				ATM.this.repaint();
+			//JLabel label = new JLabel("Hello, "+field.getText());
+		
+			}
+		};
+		button.addActionListener(listener);
+		login.addActionListener(listener);
+		panel.add(login);
+		panel.add(button);
+	
+		this.add(panel);
+		}
+	
+		public static void main(String[] args) {
+		ATM atm = new ATM();
+		atm.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		atm.setVisible(true);
+		
+		//JFrame frame = new JFrame();
+		
+		//frame.pack();
+		//frame.setVisible(true);
+
+	}
+
+}
---
+++ ATM/src/com/nibu/atm/graphic/Transactions.java
@@ -0,0 +1,7 @@
+package com.nibu.atm.graphic;
+
+import javax.swing.JPanel;
+
+public class Transactions extends JPanel{
+	
+}
